acdd1e6d328adeb633e82fdf967a68dd66e04e57
lixuf <48957019+lixuf@users.noreply.github.com>
2021-04-04 21:03:59 +0800

    Add files via upload

---
+++ 2st/rtl/clk_manger.v
@@ -0,0 +1,82 @@
+module clk_manger(
+input clk,
+input reset,
+input start,
+output clk_50_0,
+output clk_50_90,
+output clk_150_90,
+output clk_150_0,
+output reset_1_50_0,
+output reset_1_50_90
+);
+
+//开机初始化 1ms
+reg state_start;
+reg clk_start;
+reg [5:0] i;
+localparam init=1'b0;
+localparam process=1'b1;
+
+always @ (posedge clk)
+begin
+ case(state_start)
+ init:begin
+	 if(i==6'b110010)
+	  state_start<=process;
+	 else
+	 begin
+	  i=i+6'b1;
+	  clk_start<=1'b0;
+	 end
+ end
+ process:begin
+	 clk_start<=1'b1;
+ end
+ default:begin
+	 state_start<=init;
+	 clk_start<=1'b0;
+	 i<=6'b0;
+ end
+ endcase
+end
+
+
+
+
+
+
+//时钟
+//c0 50_90
+//c1 150_0
+//c2 150_90
+wire locked;
+wire clk_50_90_temp;
+wire clk_150_0_temp;
+wire clk_150_90_temp;
+pllip pll_u1(
+	.areset(),
+	.inclk0(clk),
+	.c0(clk_50_90_temp),
+	.c1(clk_150_0_temp),
+	.c2(clk_150_90_temp),
+	.locked(locked)
+	);
+assign clk_50_0=clk&clk_start&locked;
+assign clk_50_90=clk_50_90_temp&clk_start&locked;
+assign clk_150_0=clk_150_0_temp&clk_start&locked;
+assign clk_150_90=clk_150_90_temp&clk_start&locked;
+
+
+
+//复位
+///90
+wire reset_1_50_90_temp;
+sirv_gnrl_dffrs #(1) reset_dffrs_50_90(1'b0 ,reset_1_50_90_temp , clk_50_0,reset);
+sirv_gnrl_dffl #(1) reset_dffl_50_90(1'b1,reset_1_50_90_temp ,reset_1_50_90,clk_50_0);
+///0
+wire reset_1_50_0_temp;
+sirv_gnrl_dffrs #(1) reset_dffrs_50_0(1'b0 ,reset_1_50_0_temp , clk_50_90,reset);
+sirv_gnrl_dffl #(1) reset_dffl_50_0(1'b1,reset_1_50_0_temp ,reset_1_50_0,clk_50_90);
+
+
+endmodule
---
+++ 2st/rtl/top.v
@@ -0,0 +1,93 @@
+module top(
+input clk,
+input reset,
+input [15:0] ad_data,
+input ad_busy,
+input first_data,
+input start,
+
+//test
+output empty_ar,
+output full_ar,
+output [15:0] data_ar,
+input rdreq_ar,
+output clk_50_0,
+output clk_50_90,
+output clk_150_90,
+output clk_150_0,
+output reset_1_50_0,
+output reset_1_50_90,
+output data_flag,
+output ad_reset
+);
+
+/*
+wire clk_50_0;
+wire clk_50_90;
+wire clk_150_90;
+wire clk_150_0;
+wire reset_1_50_0;
+wire reset_1_50_90;
+*/
+clk_manger clk_mm(
+.clk(clk),
+.reset(reset),
+.start(start),
+.clk_50_0(clk_50_0),
+.clk_50_90(clk_50_90),
+.clk_150_90(clk_150_90),
+.clk_150_0(clk_150_0),
+.reset_1_50_0(reset_1_50_0),
+.reset_1_50_90(reset_1_50_90)
+);
+
+
+
+
+
+
+wire [2:0]  ad_os;
+wire ad_cs;
+wire ad_rd;
+//wire ad_reset;
+wire ad_convstab;
+wire [15:0] ad_ch;
+//wire data_flag;
+ad7606 vvv(
+   .clk(clk_50_0),                  //50mhz
+   .rst_n(reset_1_50),                          //同步复位
+	.ad_data(ad_data),            //ad7606 采样数据
+	.ad_busy(ad_busy),            //ad7606 忙标志位 
+   .first_data(first_data),         //ad7606 第一个数据标志位 	  
+	
+	.ad_os(ad_os),              //ad7606 过采样倍率选择
+	.ad_cs(ad_cs),              //ad7606 AD cs
+	.ad_rd(ad_rd),              //ad7606 AD data read
+   .ad_reset(ad_reset),           //ad7606 AD reset
+	.ad_convstab(ad_convstab),         //ad7606 AD convert start
+
+	.ad_ch(ad_ch),   
+
+	
+	.data_flag(data_flag)
+	);
+	
+	
+	
+//连接AD和ram控制器的fifo
+//写入50mhz，读出150mhz
+//50mhz需要与基准偏移90°
+//写时钟同步的reset
+fifoip_50_150 fifoip_ad_ram(
+	.aclr(reset_1_50_0),
+	.data(ad_ch),
+	.rdclk(clk_50_90),
+	.rdreq(rdreq_ar),
+	.wrclk(clk_150_90),
+	.wrreq(data_flag),
+	.q(data_ar),
+	.rdempty(empty_ar),
+	.wrfull(full_ar)
+	);
+
+endmodule 
